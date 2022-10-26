c1de1f4e5695e40a06073c0f435b062b4fcaf434
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-10-26 10:10:31 +0100

    Less buggy teleporter

--- entities/teleporter/teleporter.gd
+++ entities/teleporter/teleporter.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+onready var area = $Area2D
+onready var teleport = $TeleportSprite
+
+const DARK = Color(0.5, 0.5, 0.5)
+const LIGHT = Color(1, 1, 1)
+	
+class_name Teleporter
 
 var destination_level
 var destination_position
@@ -9,8 +16,13 @@ var entering_entity
 var player_in_area = false
 var is_hovered = false
 
-onready var area = $Area2D
-onready var teleport = $TeleportSprite
+func _ready():
+	teleport.visible = true
+	teleport.self_modulate = DARK
+	entering_entity = null
+	player_in_area = false
+	is_hovered = false
+	update()
 
 func _physics_process(delta):
 	if(is_hovered):
@@ -20,31 +32,31 @@ func _physics_process(delta):
 		teleport.visible = false
 	
 	if(player_in_area):
-		teleport.modulate = Color.white
+		teleport.self_modulate = LIGHT
 	else:
-		teleport.modulate = Color(0.5, 0.5, 0.5)
+		teleport.self_modulate = DARK
 
 func _on_Area2D_area_entered(area_e):
-	print("Entity entered area of teleporter")
-	player_in_area = true
-	entering_entity = area_e.get_parent()
+	if(area_e.get_parent() is Player):
+		print("Entity entered area of teleporter")
+		player_in_area = true
+		entering_entity = area_e.get_parent()
 
 func _on_Area2D_area_exited(area_e):
-	print("Entity left area of teleporter")
-	player_in_area = false
-	entering_entity = null
+	if(area_e.get_parent() is Player):
+		print("Entity left area of teleporter")
+		player_in_area = false
+		entering_entity = null
 
 func teleport(player):
-	player.position = Vector2.ZERO
-
+	if(player is Player):
+		player.position = Vector2.ZERO
 
 func _on_Area2D_mouse_entered():
 	is_hovered = true
 
-
 func _on_Area2D_mouse_exited():
 	is_hovered = false
-
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if(event is InputEventMouseButton):
--- quests/watch_birds/watch_birds.gd
+++ quests/watch_birds/watch_birds.gd
@@ -1,5 +1,6 @@
-extends Node
+extends Node2D
 
 func _on_Area2D_area_entered(area):
 	print("Area entered")
 	pass # Replace with function body.
+	
