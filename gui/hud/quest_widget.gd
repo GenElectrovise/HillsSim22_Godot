9734d74ab253bbb07b235436fe1f20a78aa67b42
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-11-02 17:09:50 +0000

    Multiple tabs for quests

--- gui/hud/quest_widget.gd
+++ gui/hud/quest_widget.gd
@@ -1,6 +1,9 @@
 extends Control
 
 onready var StudyInfo: QuestsInfo = $Foreground/Study
+onready var SocialInfo: QuestsInfo = $Foreground/Social
+onready var PersonalInfo: QuestsInfo = $Foreground/Personal
+onready var FutureInfo: QuestsInfo = $Foreground/Future
 
 func _on_visibility_changed():
 	if visible:
@@ -11,6 +14,18 @@ func refresh_text():
 	var study_quest: Quest = QuestManager.get_quest(QuestManager.study_index, QuestManager.QuestTypes.STUDY)
 	var study_objec: Objective = study_quest.current() if study_quest != null else null
 	refresh_info(study_quest, study_objec, StudyInfo)
+	
+	var social_quest: Quest = QuestManager.get_quest(QuestManager.social_index, QuestManager.QuestTypes.SOCIAL)
+	var social_objec: Objective = social_quest.current() if social_quest != null else null
+	refresh_info(social_quest, social_objec, SocialInfo)
+	
+	var personal_quest: Quest = QuestManager.get_quest(QuestManager.personal_index, QuestManager.QuestTypes.PERSONAL)
+	var personal_objec: Objective = personal_quest.current() if personal_quest != null else null
+	refresh_info(personal_quest, personal_objec, PersonalInfo)
+	
+	var future_quest: Quest = QuestManager.get_quest(QuestManager.future_index, QuestManager.QuestTypes.FUTURE)
+	var future_objec: Objective = future_quest.current() if future_quest != null else null
+	refresh_info(future_quest, future_objec, FutureInfo)
 	
 	return
 
