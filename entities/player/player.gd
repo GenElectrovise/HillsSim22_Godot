e7054a6735375f1d1abbdf4247824816d1fba488
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-11-03 18:14:36 +0000

    Added goth_girl assets and removed diagonal movement for player

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -5,8 +5,13 @@ class_name Player
 var velocity : Vector2 = Vector2()
 const reach = 10
 onready var COSTUME: Costume = $Costume
+var direction = Direction.SOUTH
 
 const POINTER_ARROW = preload("res://entities/pointer_arrow/pointer_arrow.tscn")
+
+enum Direction {
+	NORTH, SOUTH, EAST, WEST
+}
 
 func _ready():
 	if(PlayerData.has_stash):
@@ -14,22 +19,26 @@ func _ready():
 	COSTUME.reset()
 
 func handle_input():
+	handle_movement_input()
+	handle_interaction_input()
+
+func handle_movement_input():
 	velocity = Vector2.ZERO
-	
 	if(Input.is_action_pressed("player_north")):
 		velocity.y -= 1
-	if(Input.is_action_pressed("player_east")):
-		velocity.x += 1
-	if(Input.is_action_pressed("player_south")):
+	elif(Input.is_action_pressed("player_south")):
 		velocity.y += 1
-	if(Input.is_action_pressed("player_west")):
-		velocity.x -= 1#
+	elif(Input.is_action_pressed("player_east")):
+		velocity.x += 1
+	elif(Input.is_action_pressed("player_west")):
+		velocity.x -= 1
+	velocity = velocity.normalized()
+	velocity = move_and_slide(velocity * 100)
+
+func handle_interaction_input():
 	if(Input.is_action_just_pressed("player_interact")):
 		print("Trying to interact")
 		attempt_interact()
-		
-	velocity = velocity.normalized()
-	velocity = move_and_slide(velocity * 100)
 
 func look_at_mouse():
 	look_at(get_global_mouse_position())
