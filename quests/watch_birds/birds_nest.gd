b2a2857b5a59ab27aff835a2f2a9119cabd0686c
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-11-08 17:08:17 +0000

    Change hitbox detection to mouse click on watch birds

--- quests/watch_birds/birds_nest.gd
+++ quests/watch_birds/birds_nest.gd
@@ -2,7 +2,26 @@ class_name BirdsNest extends Sprite
 
 signal on_birds_watched
 
+var hovered = false
+
 func _on_Area2D_area_entered(area):
 	if area.name == "PlayerInteractArea":
 		emit_signal("on_birds_watched")
 	pass 
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if (event.type == InputEvent.MOUSE_BUTTON && event.pressed):#
+		emit_signal("on_birds_watched", position)
+		# Need instance of player to check distance?
+	pass
+
+
+func _on_Area2D_mouse_entered():
+	hovered = true
+	pass
+
+
+func _on_Area2D_mouse_exited():
+	hovered = false
+	pass
