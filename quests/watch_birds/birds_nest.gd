5e979e591d2b049f22f94b5ba884f73dd39c5b68
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-11-20 14:11:46 +0000

    Update birds_nest.gd

--- quests/watch_birds/birds_nest.gd
+++ quests/watch_birds/birds_nest.gd
@@ -4,12 +4,6 @@ signal on_birds_watched
 
 var hovered = false
 
-func _init():
-	print("init birdsnest")
-
-func _ready():
-	print("nest ready")
-
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("player_interact") && hovered:
 		push_warning("Not checking player distance interact!")
