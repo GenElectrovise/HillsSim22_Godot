e060b81095b69a7661a4341564cb2d6e1bed3b44
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-11-20 10:07:15 +0000

    Seperated menus

--- gui/title/title.gd
+++ gui/title/title.gd
@@ -1,25 +1,11 @@
 extends Node
 
-class_name TitleScreen
-
-onready var MAIN: Control = $Main
-onready var NEW_GAME: Control = $NewGame
-onready var CUSTOMISE: Control = $CustomiseCharacter
+onready var TITLE_SCREEN = get_parent()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Global.switch_scene(Levels.lookup("outside"))
-	change_menu("main")
+	pass
 
-func change_menu(name):
-	
-	MAIN.visible = false
-	NEW_GAME.visible = false
-	CUSTOMISE.visible = false
-	
-	if(name == "main"):
-		MAIN.visible = true
-	elif(name == "newgame"):
-		NEW_GAME.visible = true
-	if(name == "customise"):
-		CUSTOMISE.visible = true
+func _on_NewGameButton_pressed():
+	TITLE_SCREEN.change_menu("newgame")
+	pass 
